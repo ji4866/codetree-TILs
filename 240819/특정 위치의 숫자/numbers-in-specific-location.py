918e81bfec7d84984cb43f170aedce3c87362082
ji4866 <65593398+ji4866@users.noreply.github.com>
2024-08-19 21:48:37 +0900

    Title: 특정 위치의 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240819/특정 위치의 숫자/numbers-in-specific-location.py
+++ 240819/특정 위치의 숫자/numbers-in-specific-location.py
@@ -1,16 +1,6 @@
-a, b, c, d, e, f, g, h, i, j = map(int, input().split(' '))
+num_list = list(map(int, input().split(' ')))
 
-num_list = []
-num_list.append(a)
-num_list.append(b)
-num_list.append(c)
-num_list.append(d)
-num_list.append(e)
-num_list.append(f)
-num_list.append(g)
-num_list.append(h)
-num_list.append(i)
-num_list.append(j)
+
 
 sum = num_list[2]+num_list[4]+num_list[9]
 print(sum)
