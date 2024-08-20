a4fc2760949d23f5aa2974f84a621390ff9b1fff
ji4866 <65593398+ji4866@users.noreply.github.com>
2024-08-20 14:06:33 +0900

    Title: 나눗셈의 나머지, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/나눗셈의 나머지/remainder-of-division.py
+++ 240820/나눗셈의 나머지/remainder-of-division.py
@@ -2,18 +2,19 @@ a, b = map(int, input().split())
 arr = {}
 sum = 0
 
-#print(a,'/',b,'=', end='')
 
 while a>1:
   count = 1
   #print(a,'/',b,'=', end='')
-  if a%b not in arr:
-    arr[a%b] = count
+  remain = a%b
+
+  if remain not in arr:
+    arr[remain] = count
   else:
-    count += 1
-    arr[a%b] = count
+    count = arr[remain]+1
+    arr[remain] = count
   a = int(a/b)
-  #print(a, '...',a%b)
+  #print(a, '...',remain)
 
 for keys in arr:
   sum += arr[keys]**2
